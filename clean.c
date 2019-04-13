03a499f27419ff9f6b7d94edcfe8ff916b29a933
Sophia <sgigliotti@westmont.edu>
2019-04-13 13:17:11 -0700

    sending name work

--- clean.c
+++ clean.c
@@ -13,6 +13,11 @@
 
 #define SERVER "10.115.20.250"
 #define PORT 28900
+#define BUFSIZE = 1000;
+#define NAMESIZE = 1000;
+char *name;
+char *accesspoint;
+char *buffer;
 
 /*
  * connects to the server and prints errors if any is encountered
@@ -43,6 +48,52 @@ int connect2v4stream(char *IP_adr){
 	return sockd;
 }
 
+/*
+ * replies to sent messages with where we are
+ */
+char * getuser(int fd){
+	name = malloc(BUFSIZ+1);
+	printf("Please enter a user-name:\n");
+	fgets(name, BUFSIZ, stdin);
+	send(fd, name, BUFSIZ, 0);
+	
+	return name;
+}
+
+/*
+ * get access point
+ */
+char * getap(){
+	accesspoint =  malloc(BUFSIZ+1);
+	accesspoint = "vl-1a-wap3\n";
+
+	return accesspoint;
+}
+
+int recvandtell(int fd){
+	int ret;
+	buffer = malloc(BUFSIZ+1);
+	memset(buffer, 0, BUFSIZ);
+	ret = recv(fd, buffer, BUFSIZ+1, 0);
+
+	if(ret == -1){
+		if(errno == EAGAIN){
+		} 
+	} else if(ret == 0){
+		exit(0);
+	} else{
+		char * combo = malloc(BUFSIZ+1);
+		int c = 0;
+		strcat(combo, " ");
+		strcat(combo, accesspoint);
+		send(fd, combo, sizeof(combo+1),0);
+		printf("%s ",combo);
+		free(combo);
+	}
+
+	return ret;
+}
+
 void update_time(int sockd){
 	int ret;
 	char *message = "GET /?i=tleslie&uptime=60 HTTP/1.1\r\n"  //make sure to remove the hard-coded stuff
@@ -62,12 +113,15 @@ int main(){
 
 	sockd = connect2v4stream(SERVER);
 
+	getuser(sockd);
+	getap();
+
 	update_time(sockd);
 
 	//prepares select
 	setsockopt(sockd, SOL_SOCKET, SO_RCVTIMEO, &timev, sizeof(timev));
 
-
+	recvandtell(sockd);
 
 	/*
 	 * still need to set up select so it works to listen for attacks and send out who are you requests to
