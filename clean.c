f7d036980e87142c1b574f6f92c3e801b8d94b46
Sophia <sgigliotti@westmont.edu>
2019-04-14 21:00:52 -0700

    select kinda working

--- clean.c
+++ clean.c
@@ -24,24 +24,28 @@ char *buffer;
  *
  * the error handling needs to be changed because we will be expecting many errors when trying to connect
  */
-int connect2v4stream(char *IP_adr){
+int connect2v4stream(char *IP_adr)
+{
 	struct sockaddr_in server;
 	int sockd, ret;
 
-	sockd = socket(AF_INET,SOCK_STREAM,0);
-	if(sockd == -1){
+	sockd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockd == -1)
+	{
 		printf("%s\n", strerror(errno));
 	}
 	server.sin_family = AF_INET;
 	server.sin_port = htons(PORT); //converts port number
 
 	ret = inet_pton(AF_INET, IP_adr, &server.sin_addr);
- 	if(ret == -1){
- 		printf("%s\n", strerror(errno));
- 	}
+	if (ret == -1)
+	{
+		printf("%s\n", strerror(errno));
+	}
 
- 	ret = connect(sockd, (struct sockaddr *)&server, sizeof(server));
-	if(ret == -1){
+	ret = connect(sockd, (struct sockaddr *)&server, sizeof(server));
+	if (ret == -1)
+	{
 		printf("%s\n", strerror(errno));
 	}
 
@@ -51,66 +55,77 @@ int connect2v4stream(char *IP_adr){
 /*
  * replies to sent messages with where we are
  */
-char * getuser(int fd){
-	name = malloc(BUFSIZ+1);
+char *getuser(int fd)
+{
+	name = malloc(BUFSIZ + 1);
 	printf("Please enter a user-name:\n");
 	fgets(name, BUFSIZ, stdin);
 	send(fd, name, BUFSIZ, 0);
-	
+
 	return name;
 }
 
 /*
  * get access point
  */
-char * getap(){
-	accesspoint =  malloc(BUFSIZ+1);
+char *getap()
+{
+	accesspoint = malloc(BUFSIZ + 1);
 	accesspoint = "vl-1a-wap3\n";
 
 	return accesspoint;
 }
 
-int recvandtell(int fd){
+int recvandtell(int fd)
+{
 	int ret;
-	buffer = malloc(BUFSIZ+1);
+	buffer = malloc(BUFSIZ + 1);
 	memset(buffer, 0, BUFSIZ);
-	ret = recv(fd, buffer, BUFSIZ+1, 0);
+	ret = recv(fd, buffer, BUFSIZ + 1, 0);
 
-	if(ret == -1){
-		if(errno == EAGAIN){
-		} 
-	} else if(ret == 0){
-		exit(0);
-	} else{
-		char * combo = malloc(BUFSIZ+1);
+	if (ret == -1)
+	{
+		if (errno == EAGAIN)
+		{
+		}
+	}
+	else if (ret == 0)
+	{
+		
+	}
+	else
+	{
+		char *combo = malloc(BUFSIZ + 1);
 		int c = 0;
 		strcat(combo, " ");
 		strcat(combo, accesspoint);
-		send(fd, combo, sizeof(combo+1),0);
-		printf("%s ",combo);
+		send(fd, combo, sizeof(combo + 1), 0);
+		printf("%s ", combo);
 		free(combo);
 	}
 
 	return ret;
 }
 
-void update_time(int sockd){
+void update_time(int sockd)
+{
 	int ret;
-	char *message = "GET /?i=tleslie&uptime=60 HTTP/1.1\r\n"  //make sure to remove the hard-coded stuff
-				    "Host: pilot.westmont.edu:28900\r\n\r\n";
+	char *message = "GET /?i=tleslie&uptime=60 HTTP/1.1\r\n" //make sure to remove the hard-coded stuff
+					"Host: pilot.westmont.edu:28900\r\n\r\n";
 
 	ret = write(sockd, message, strlen(message));
-	if(ret == -1){
+	if (ret == -1)
+	{
 		printf("%s\n", strerror(errno));
 		exit(errno);
 	}
 }
 
-int main(){
+int main()
+{
 	int sockd;
-	struct timeval timev;
-	fd_set readfds;
-
+	int s;
+	
 	sockd = connect2v4stream(SERVER);
 
 	getuser(sockd);
@@ -118,16 +133,61 @@ int main(){
 
 	update_time(sockd);
 
-	//prepares select
-	setsockopt(sockd, SOL_SOCKET, SO_RCVTIMEO, &timev, sizeof(timev));
+	fd_set write, activeWrite;
+	struct timeval time;
+	int quit = 0;
 
-	recvandtell(sockd);
+	//set time for select to switch after 1 second
+	//so that messages can be received in real time
+	//Set up recv timeout for .5 sec
+  	time.tv_sec = 0;
+  	time.tv_usec = 1000 * 1000;
+  	setsockopt(sockd, SOL_SOCKET, SO_RCVTIMEO, &time, sizeof(time));
 
+	while (!quit)
+	{
+
+		//reset the fd select variable each iteration through the loop
+		FD_ZERO(&write);
+		FD_SET(0, &write);
+		FD_SET(sockd, &write);
+
+		s = select(sockd + 1, &write, NULL, NULL, &time);
+
+		//if the fd is set, receive the message
+		if (FD_ISSET(sockd, &write))
+		{
+			printf("Inside \n");
+			recvandtell(sockd);
+			
+			sleep(9);
+			printf("Outside \n");
+		}
+
+		//otherwise send "who are you" messages to the server
+		else{
+
+			int sentMsg;
+			printf("Inside2 \n");
+			//allocate memory for sending messages
+			buffer = malloc(BUFSIZ + 1);
+			buffer = "Who are you?\n";
+			sentMsg = send(sockd, buffer, strlen(buffer), 0);
+			printf("Outside2 \n");
+			free(buffer);
+
+		}
+	}
+
+	FD_CLR(0, &activeWrite);
+
+	
 	/*
 	 * still need to set up select so it works to listen for attacks and send out who are you requests to
 	 * the range of IP numbers (64.136.178.1 - 64.136.178.254)
 	 */
 
-
+	printf("boo \n");
 	close(sockd);
+	printf("boo2 \n");
 }
