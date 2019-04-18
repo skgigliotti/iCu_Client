f144bac49448d4f99eff6f368a97265fbea6da3b
Tanner Leslie <tleslie@westmont.edu>
2019-04-17 23:18:50 -0700

    separated the project into three separate files to be able to run the whole thing

---
+++ a.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <ncurses.h>
+
+#define SERVER "10.115.20.250"
+#define PORT 28900
+
+char * name, * accesspoint;
+
+void make_socket(){ //server side
+    struct sockaddr_in address;
+    socklen_t address_len;
+    int ret;
+
+    address_len = sizeof(struct sockaddr_in);
+
+	int serverfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    ret = bind(serverfd, (struct sockaddr *)&address, sizeof(address));
+    if(ret == -1){
+   		printf("%s\n", strerror(errno));
+   	}
+
+	ret = listen(serverfd, 1);
+	if(ret == -1){
+ 		printf("%s\n", strerror(errno));
+ 	}
+
+	int new_socket = accept(serverfd, (struct sockaddr *)&address, &address_len);
+	char *found_msg = name;
+	strcat(found_msg, " ");
+	strcat(found_msg, accesspoint);
+	strcat(found_msg, "\n");
+//	char *foo = malloc(1000);
+//	read(new_socket, foo, 1000);
+	write(new_socket, found_msg, strlen(found_msg));
+	close(serverfd);
+}
+
+char *getap() //server side
+{
+	accesspoint = "34:85:84:0e:6e:e5"; //"vl-1a-wap3\n";
+
+	return accesspoint;
+}
+
+char *getuser() //both
+{
+	name = malloc(BUFSIZ + 1);
+	printf("Please enter a user-name:\n");
+	fgets(name, BUFSIZ, stdin);
+	name = strtok(name, "\n");
+	return name;
+}
+
+int main(){
+
+	getuser();
+	getap();
+
+	while(1){
+		make_socket();
+	}
+
+	return 0;
+}
---
+++ c.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <ncurses.h>
+
+#define SERVER "10.115.20.250"
+#define PORT 28900
+
+char * name;
+
+int connect2v4stream(char *IP_adr){ //both
+	struct sockaddr_in server;
+	int sockd, ret;
+
+	sockd = socket(AF_INET,SOCK_STREAM,0);
+	if(sockd == -1){
+		printf("%s\n", strerror(errno));
+		return sockd;
+	}
+	server.sin_family = AF_INET;
+	server.sin_port = htons(PORT); //converts port number
+
+	ret = inet_pton(AF_INET, IP_adr, &server.sin_addr);
+ 	if(ret == -1){
+ 		printf("%s\n", strerror(errno));
+ 		return sockd;
+ 	}
+
+ 	ret = connect(sockd, (struct sockaddr *)&server, sizeof(server));
+	if(ret == -1){
+		printf("%s\n", strerror(errno));
+		return sockd;
+	}
+
+	return sockd;
+}
+
+void update_time(int sockd){ //either
+	int ret;
+	char *message;
+
+	message = malloc(100);
+	sprintf(message, "GET /?i=%s&uptime=60 HTTP/1.1\r\nHost: pilot.westmont.edu:28900\r\n\r\n", name);
+
+	ret = write(sockd, message, strlen(message));
+	if(ret == -1){
+		printf("%s\n", strerror(errno));
+		exit(errno);
+	}
+	return;
+}
+
+char *getuser()
+{
+	name = malloc(BUFSIZ + 1);
+	printf("Please enter a user-name:\n");
+	fgets(name, BUFSIZ, stdin);
+	name = strtok(name, "\n");
+	return name;
+}
+
+int main(){
+	int sockd;
+	struct timeval start, now;
+	double elapsed;
+
+	sockd = connect2v4stream(SERVER);
+
+	getuser();
+
+	gettimeofday(&start,NULL);
+
+	while(1){
+		gettimeofday(&now, NULL);
+		elapsed = now.tv_sec - start.tv_sec;
+		if( ((int) elapsed) % 60 == 0 ){
+			update_time(sockd);
+		}
+	}
+
+
+	return 0;
+}
